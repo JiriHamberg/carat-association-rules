fc1918f71e58ddff4c5f98d84667baa5e5d4df7a
Jiri Hamberg <jiri.hamberg@gmail.com>
2018-03-20 19:09:37 +0200

    Cleaned up previous changes

--- analysis-engine/src/main/scala/models/Sample.scala
+++ analysis-engine/src/main/scala/models/Sample.scala
@@ -58,36 +58,6 @@ object Sample {
 }
 
 
-case class SampleDiscretization(
-  rate: Array[Double],
-  cpu: Array[Double],
-  distance: Array[Double],
-	temp: Array[Double],
-	voltage: Array[Double],
-	screen: Array[Double],
-	wifiStrength: Array[Double],
-	wifiSpeed: Array[Double]
-)
-
-
-/*object SampleDiscretization {
-  def fromSamples(samples: RDD[Sample], nBins: Int = 4)(implicit sqlContext: SQLContext): SampleDiscretization = {
-
-      SampleDiscretization(
-        rate = Discretization.getQuantiles(samples.map(s => s.rate), nBins),
-        cpu = Discretization.getQuantiles(samples.map(s => s.cpu), nBins, partial = Discretization.cpuPartial),
-        distance = Discretization.getQuantiles(samples.map(s => s.distance), nBins, partial = Discretization.travelDistancePartial),
-        temp = Discretization.getQuantiles(samples.map(s => s.temp), nBins, partial = Discretization.temperaturePartial),
-        voltage = Discretization.getQuantiles(samples.map(s => s.voltage), nBins),
-        screen = Discretization.getQuantiles(samples.map(s => s.screen), nBins, partial = Discretization.screenPartial),
-        wifiStrength = Discretization.getQuantiles(samples.map(s => s.wifiStrength), nBins, partial = Discretization.wifiStrengthPartial),
-        wifiSpeed = Discretization.getQuantiles(samples.map(s => s.wifiSpeed), nBins, partial = Discretization.wifiSpeedPartial)
-      )
-  }
-
-}*/
-
-
 object Discretization {
 
 	val ratePartial: PartialFunction[Double, Option[String]] = {
@@ -99,7 +69,6 @@ object Discretization {
 
   val cpuPartial: PartialFunction[Double, Option[String]] = {
     case x if x < 0.0 => None
-    //case x if x > 100.0 => None
     case x if x > 1.0 => None
     case x if x.isNaN => None
 	}
@@ -156,14 +125,11 @@ object Discretization {
 
 		val percentiles = (for(i <- 1 to (buckets - 1)) yield (1.0 / buckets) * i).toArray
 		val notDefined = data.filter(x => !partial.isDefinedAt(x))
-    //val quantiles = notDefined.toDF("col").stat.approxQuantile("col", percentiles, relativeError)
 
     try {
-    	//val Row(min: Double, max: Double) = notDefined.agg(min("col"), max("col"))
     	val dataFrame = notDefined.toDF("col").cache()
     	val Row(minValue: Double, maxValue: Double) = dataFrame.agg(min("col"), max("col")).head
     	(dataFrame.stat.approxQuantile("col", percentiles, relativeError), minValue, maxValue)
-		  //(notDefined.toDF("col").stat.approxQuantile("col", percentiles, relativeError), min, max)
     } catch{
       //see https://issues.apache.org/jira/browse/SPARK-21550
       case ex: java.util.NoSuchElementException => (Array[Double](), 0, 0)
@@ -188,33 +154,24 @@ object Discretization {
 		val bins = scala.collection.mutable.Map[String, Seq[Double]]()
 
 		// RATE
-		//val rates = samples.map(_.rate)
-		//rates.cache()
-		//val rateMin = 
-		//val rateQuantiles = getQuantiles(rates, 4)
-		//val rateQuantiles = getQuantiles(samples.map(_.rate), 4)
 		val (rateQuantiles, rateMin, rateMax) = getQuantiles(samples.map(_.rate), 4, partial = ratePartial)
-		//bins("rate") = Seq(0.0) ++ rateQuantiles.toSeq ++ Seq(1.0)
 		bins("rate") = Seq(rateMin) ++ rateQuantiles.toSeq ++ Seq(rateMax)
 
-		//val cpuQuantiles = getQuantiles(samples.map(_.cpu), 4, partial = cpuPartial)
+		// CPU
 		val (cpuQuantiles, cpuMin, cpuMax) = getQuantiles(samples.map(_.cpu), 4, partial = cpuPartial)
     if(!excluded.contains("cpu"))
 		  bins("cpu") = Seq(cpuMin) ++ cpuQuantiles.toSeq ++ Seq(cpuMax)
 
 		// TEMPERATURE
-		//val temperatureQuantiles = getQuantiles(samples.map(_.temp), 4, partial = temperaturePartial)
 		val (temperatureQuantiles, tempMin, tempMax) = getQuantiles(samples.map(_.temp), 4, partial = temperaturePartial)
 		if(!excluded.contains("temperature"))
     bins("temperature") = Seq(tempMin) ++ temperatureQuantiles.toSeq ++ Seq(tempMax)
 
 		// VOLTAGE
-		//val voltageQuantiles = getQuantiles(samples.map(_.voltage), 3)
 		val (voltageQuantiles, voltageMin, voltageMax) = getQuantiles(samples.map(_.voltage), 3)
 		bins("voltage") = Seq(voltageMin) ++ voltageQuantiles.toSeq ++ Seq(voltageMax)
 
 		// SCREEN
-		//val screenQuantiles = getQuantiles(samples.map(_.screen), 4, partial = screenPartial)
 		val (screenQuantiles, screenMin, screenMax) = getQuantiles(samples.map(_.screen), 4, partial = screenPartial)
 		if(!excluded.contains("screen"))
       bins("screen") = Seq(screenMin) ++ screenQuantiles.toSeq ++ Seq(screenMax)
@@ -224,13 +181,11 @@ object Discretization {
 		// NETWORK TYPE
 
 		//WIFI STRENGTH
-		//val wifiStrengthQuantiles = getQuantiles(samples.map(_.wifiStrength), 4, partial = wifiStrengthPartial)
 		val (wifiStrengthQuantiles, wifiStrengthMin, wifiStrengthMax) = getQuantiles(samples.map(_.wifiStrength), 4, partial = wifiStrengthPartial)
 		if(!excluded.contains("wifiStrength"))
       bins("wifiStrength") = Seq(wifiStrengthMin) ++ wifiStrengthQuantiles.toSeq ++ Seq(wifiStrengthMax)
 
 		//WIFI SPEED
-		//val wifiSpeedQuantiles = getQuantiles(samples.map(_.wifiSpeed), 4, partial = wifiSpeedPartial)
 		val (wifiSpeedQuantiles, wifiSpeedMin, wifiSpeedMax) = getQuantiles(samples.map(_.wifiSpeed), 4, partial = wifiSpeedPartial)
 		if(!excluded.contains("wifiSpeed"))
       bins("wifiSpeed") = Seq(wifiSpeedMin) ++ wifiSpeedQuantiles.toSeq ++ Seq(wifiSpeedMax)
